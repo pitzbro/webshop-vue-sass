2f3e30613cb336964f38f368f2db4459f0ad3e40
pitzbro <asioren@gmail.com>
2022-01-20 20:10:22 +0200

    first commit

--- src/components/cats/cat-card.vue
+++ src/components/cats/cat-card.vue
@@ -1,11 +1,14 @@
 <template>
   <section class="card-cat card">
+    <h2 v-html="logo"></h2>
     <img class="cat-image" :src="`https://cataas.com/cat/${tags[tagNum]}`"/>
     <div class="cat-tag">{{tags[tagNum]}}</div>
   </section>
 </template>
 
 <script>
+
+import { miscellaneous, logos } from "../../services/svg.service";
 export default {
 
   name: 'CatCard',
@@ -16,6 +19,8 @@ export default {
 
   data: () => {
     return {
+      search: miscellaneous.search,
+      logo: logos.logoFull,
       catImage: null,
       tags: ['cute', 'fail', 'stupid', 'funny', 'serious', 'white', 'gif', 'happy', 'sleeping', 'grumpy', 'old', 'fat']
     }
