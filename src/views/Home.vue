a6b38381925ad4c42fb5bb7f49073beb8626062c
pitzbro <asioren@gmail.com>
2022-01-21 15:39:15 +0200

    added header content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="home card-grid site-block">
+  <header class="main-header">
     <div v-html="logo"></div>
     <nav>
       <ul class="main-nav">
@@ -10,12 +10,21 @@
         <li><a href="#">Contact</a></li>
       </ul>
     </nav>
+    <img src="~@/assets/images/header1920.jpg" alt="">
     <div class="case-nav">
       <span class="case-title">
       Museum of Modern Art Moma
       </span>
       New York
       <a href="#" class="btn-link">Case study</a>
+    </div>
+    <div class="slide-info">
+      <span class="slide-name">Light consulting & sales</span>
+      <span class="slide-pager">1/5</span>
+      <span class="slide-pager-arrows">
+        <button class="pager next"></button>
+        <button class="pager prev"></button>
+      </span>
     </div>
   </header>
 </template>
