e127194430155e38b6e14cdb83d3cc817a3d7fe6
pitzbro <asioren@gmail.com>
2022-01-21 14:50:29 +0200

    html

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
 <template>
   <div id="app" class="main-layout">
-    <header class="main-header full main-layout" id="nav">
-      <img class="logo" src="~@/assets/logo/cat-logo.gif" />
-      <h1>My cat website</h1>
-      <p>We’re all cat lovers. That’s why we’re here. But have you ever stopped to wonder why we find cats so incredible loveable?</p>
-      <p>With Valentine’s Day just around the corner, it seemed the perfect time to explore our fascination with our self-domesticated feline friends.</p>
-
-    </header>
     <router-view />
     <footer class="main-footer full main-layout">
       <small>all rights reserved to cats @2019</small>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <main class="home card-grid site-block">
-    <cat-card v-for="(cat, index) in 12" :tagNum="index" />
-  </main>
+  <header class="home card-grid site-block">
+    <div v-html="logo"></div>
+    <nav>
+      <ul class="main-nav">
+        <li><a href="#">About us</a></li>
+        <li><a href="#">services</a></li>
+        <li><a href="#">Brands</a></li>
+        <li><a href="#">Refernces</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+    <div class="case-nav">
+      <span class="case-title">
+      Museum of Modern Art Moma
+      </span>
+      New York
+      <a href="#" class="btn-link">Case study</a>
+    </div>
+  </header>
 </template>
 
 <script>
 
 // COMPONENTS
 import CatCard from '@/components/cats/cat-card.vue'
+import { miscellaneous, logos } from "../services/svg.service";
 
 export default {
   name: 'home',
   components: {
-    CatCard
-  }
+    },
+
+  data: () => {
+    return {
+      logo: logos.logoFull,
+    }
+  },
 }
 </script>
